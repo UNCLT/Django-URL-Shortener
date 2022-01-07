a0facceff4b8942fa88955880016f1d4c924ec94
Anatoly Kirillov <drdizel@mail.ru>
2022-01-07 23:18:49 +0300

    Update admin panel

--- shortlink/admin.py
+++ shortlink/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Link, Domain
 
-admin.site.register(Link)
-admin.site.register(Domain)
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ['user', 'source_link', 'slug', 'date_created', 'date_modified']
+    ordering = ['-date_modified']
+
+
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ['user', 'domain_name', 'date_created']
+    ordering = ['-date_created']
+
+
+admin.site.register(Link, LinkAdmin)
+admin.site.register(Domain, DomainAdmin)
